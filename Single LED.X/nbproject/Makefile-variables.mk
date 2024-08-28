45f1730e54f52ad128b7ffe9c2cd8a8252cecb3a
vinayak <vinaybelagavi.2123@gmail.com>
2024-08-28 15:18:19 +0530

    pushing L

---
+++ Single LED.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Single_LED.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Single_LED.X.production.hex
